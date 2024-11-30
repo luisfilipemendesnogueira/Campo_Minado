226ff0f7f22ee5a35c41ba9c959388dee37cdbc8
Luis Filipe <luisfilipemendesnogueira@gmail.com>
2024-11-29 21:35:01 -0300

    update

---
+++ campo_minado/src/luisfilipe/modelo/Aplicacao.java
@@ -0,0 +1,12 @@
+package luisfilipe.modelo;
+
+import luisfilipe.video.TabuleiroConsole;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+		new TabuleiroConsole(tabuleiro);
+	}
+}
